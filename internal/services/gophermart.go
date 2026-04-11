f7ea7528f14b33d6e7a340fb7fa8364814cbdecd
Grigorii Vakhnin <>
2026-04-11 17:42:37 +0300

    Правки

--- internal/services/gophermart.go
+++ internal/services/gophermart.go
@@ -148,14 +148,15 @@ func (s *GophermartService) SaveOrder(ctx context.Context, orderNumber string, u
 	// Проверяем что заказ еще не добавлен
 	id, err := s.queries.GetUserIDByOrder(ctx, orderNumber)
 
-	if err != nil && err != sql.ErrNoRows {
-		return err
+	if err == nil {
+		if id == userID {
+			return ErrOrderByUserLoaded
+		}
+		return ErrOrderLoaded //Заказ есть, но загружен другим пользователем
 	}
 
-	if id == 0 && userID == 0 && err != sql.ErrNoRows {
-		return ErrOrderByUserLoaded
-	} else if id != 0 { // Заказ есть, но загружен другим пользователем
-		return ErrOrderLoaded
+	if err != sql.ErrNoRows {
+		return err
 	}
 
 	// Сохраняем номер заказа в БД
